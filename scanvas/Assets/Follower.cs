13e7e3e1eee6572102c7701c8d4e559cca4a78ae
drmbt <dreambaitrecordings@gmail.com>
2017-02-16 03:07:43 -0600

    add steamvr first pass at spectator camera

---
+++ scanvas/Assets/Follower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour {
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Vector3 offset = Vector3.up * 5 + Vector3.back * 5;
+
+    [SerializeField]
+    private float damp = 3f;
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var targetPos = target.position + target.TransformPoint(offset);
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * damp);
+        transform.LookAt(target);
+	}
+}
